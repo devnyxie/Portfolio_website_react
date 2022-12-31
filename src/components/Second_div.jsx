7b93906cae34258e0dbe3d127c4cc2ace66b2a87
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-12-31 02:51:58 +0100

    alignment fix, step towars responsiveness (second block)

--- src/components/Second_div.jsx
+++ src/components/Second_div.jsx
@@ -2,15 +2,17 @@ import { Row, Col } from "react-bootstrap";
 import pfp from "../data/pfp.jpg";
 export const Second_div = () => {
   return (
-    <div className="bg-dark half-height" id="about">
-      <div className="d-flex justify-content-center half-height text-light ">
-        {/* <div className="bg-dark w-75">h1</div> */}
-        <Row className="w-75 d-flex justify-content-center align-items-center">
-          <Col xs={4} className="d-flex justify-content-center">
+    <div
+      className="bg-dark half-height d-flex align-items-center justify-content-center"
+      id="about"
+    >
+      <div className="text-light">
+        <Row className="container d-flex justify-content-center align-items-center">
+          <Col xs={4}>
             <img
               src={pfp}
-              className="social"
-              style={{ borderRadius: "50%", maxHeight: "350px" }}
+              className="social w-100"
+              style={{ borderRadius: "50%" }}
             />
           </Col>
           <Col xs={7}>
@@ -25,19 +27,24 @@ export const Second_div = () => {
               At the moment, I'm finishing EPICODE's Master Camp to become a
               Full-Stack Developer, but I'm already in search for a good job.
               <br />
-              <br />
-              My front-end skills:
-              <ul>
-                <li>HTML+CSS</li>
-                <li>-JavaScript/TypeScript</li>
-                <li>React.js/Redux</li>
-              </ul>
-              My back-end skills:
-              <ul>
-                <li>Express.js</li>
-                <li>MongoDB</li>
-                <li>PostgreSQL</li>
-              </ul>
+              <div className="d-flex justify-content-around">
+                <div>
+                  <h5>My front-end skills:</h5>
+                  <ul>
+                    <li>HTML+CSS</li>
+                    <li>JavaScript/TypeScript</li>
+                    <li>React.js/Redux</li>
+                  </ul>
+                </div>
+                <div>
+                  <h5>My back-end skills:</h5>
+                  <ul>
+                    <li>Express.js</li>
+                    <li>MongoDB</li>
+                    <li>PostgreSQL</li>
+                  </ul>
+                </div>
+              </div>
             </p>
           </Col>
         </Row>
